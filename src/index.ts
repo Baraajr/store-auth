fe7249019290dc4d4abe5ba2393063f9313e73e0
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-03-05 11:53:53 +0200

    middlewares for logging and security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app: Application = express();
+
+// logger
+app.use(morgan('dev'));
+
+// http security middleware
+app.use(helmet());
+
+// body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // form submission middleware
+
+// rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'true', // return rate limit info`RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+app.use(limiter);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
