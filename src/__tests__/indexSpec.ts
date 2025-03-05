7487b8282834aa12127e3c685d4ec79809eec378
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-03-05 11:38:46 +0200

    added test configurations

---
+++ src/__tests__/indexSpec.ts
@@ -0,0 +1,12 @@
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+
+describe('test basic endpoints', () => {
+  it('GET /', async () => {
+    const response = await request.get('/');
+    expect(response.status).toBe(200);
+    // expect(response.message).toBe('Hello, World!');
+  });
+});
