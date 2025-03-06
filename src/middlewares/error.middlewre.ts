5dbd6c409dde9232dec51d037387b4e2dac9e9de
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-03-06 11:57:31 +0200

    updated error middleware

--- src/middlewares/error.middlewre.ts
+++ src/middlewares/error.middlewre.ts
@@ -1,8 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Error from '../interfaces/error.interface';
-const errorMiddleware = (err: Error, req: Request, res: Response) => {
+
+const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const status = err.status || 500;
-  const message = err.message || 'something went wrong';
+  const message = err.message || 'Something went wrong';
+
   res.status(status).json({ message });
 };
 
