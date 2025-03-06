015a082333b13ce7a77e753c84e5e364d57a2bb1
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-03-06 11:59:47 +0200

    updated error middleware

--- src/middlewares/error.middlewre.ts
+++ src/middlewares/error.middlewre.ts
@@ -5,6 +5,7 @@ const errorMiddleware = (
   err: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   const status = err.status || 500;
